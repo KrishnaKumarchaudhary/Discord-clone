a68b3751c5029c84fe0454eb116cbe9c9c64f286
Krishna kumar chaudhary <ch.krishnakumar01@gmail.com>
2024-03-03 09:52:22 +0530

    Authentication completion

---
+++ discord-backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const authRoutes = require("./routers/authRoutes");
+
+const PORT = process.env.PORT || process.env.API_PORT;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// register the routers
+app.use("/api/auth", authRoutes);
+
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`server is listening on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("database connec tion failed. server is not started");
+  });
